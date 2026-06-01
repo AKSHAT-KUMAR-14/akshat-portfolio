d6f206e14b1bbbd6bcbce8207277a775e2ae76ef
Akshat Kumar <akshatkumar0110@gmail.com>
2026-06-01 23:06:20 +0530

    Initial portfolio project setup

---
+++ src/data/portfolio-information.js
@@ -0,0 +1,7 @@
+export const projects = [
+  {
+    title: "AKSHEDI",
+    description: "HRIS Platform",
+    tech: ["React", "Node.js"],
+  },
+];
---
+++ src/data/portfolioData.js
@@ -0,0 +1,7 @@
+export const profile = {
+  name: "Akshat Kumar",
+  role: "Data Science & Software Developer",
+  location: "Kanpur, Uttar Pradesh",
+};
+
+export const projects = [];
