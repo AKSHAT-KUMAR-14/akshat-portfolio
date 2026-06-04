51c89937202838001238a3923dcd72e45c7b48cc
Akshat Kumar <akshatkumar0110@gmail.com>
2026-06-04 18:38:33 +0530

    Create portfolio layout skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import "./styles/layout.css";
+
 function App() {
   return (
-    <div>
-      <h1>Akshat Kumar Portfolio</h1>
+    <div className="app-container">
+      <div className="main-layout">
+
+        <div
+          style={{
+            background: "#1e1e1f",
+            minHeight: "700px",
+            borderRadius: "20px",
+          }}
+        >
+          Sidebar
+        </div>
+
+        <div
+          style={{
+            background: "#1e1e1f",
+            minHeight: "700px",
+            borderRadius: "20px",
+          }}
+        >
+          Main Content
+        </div>
+
+      </div>
     </div>
   );
 }
